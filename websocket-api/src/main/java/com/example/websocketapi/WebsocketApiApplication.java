e3c40e565dc0e960f8cbe9a865d34ddd10f7c9f8
Arthur Azevedo <arthuraz.1990@gmail.com>
2024-05-22 09:59:32 -0300

    Commit inicial do projeto.

---
+++ websocket-api/src/main/java/com/example/websocketapi/WebsocketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketApiApplication.class, args);
+	}
+
+}
---
+++ websocket-api/src/test/java/com/example/websocketapi/WebsocketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
